b12d108d6bb25939160c05a048b15561cedb0566
Zach Damit <github.pueblo686@passmail.net>
2025-10-14 08:59:23 -0500

    Revert about.php

--- about.php
+++ about.php
@@ -1,6 +1,5 @@
 <?php
-require_once __DIR__ . '/../config/config.php';
-
+require_once dirname(__DIR__) . '/config/bootstrap_env.php';
 $contactEmail = $_ENV['CONTACT_EMAIL'] ?? getenv('CONTACT_EMAIL') ?? '';
 ?>
 <!DOCTYPE html>
