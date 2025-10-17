690c429eda1c490c93882be896910b633d0bbb05
Zach Damit <github.pueblo686@passmail.net>
2025-10-17 10:43:09 -0500

    Harden Steam login host detection

--- steam_callback.php
+++ steam_callback.php
@@ -2,7 +2,38 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$oid = new LightOpenID(parse_url(BASE_URL, PHP_URL_HOST));
+$hostFromEnv = '';
+$googleRedirect = $_ENV['GOOGLE_REDIRECT_URI'] ?? getenv('GOOGLE_REDIRECT_URI') ?: '';
+
+if ($googleRedirect !== '') {
+    $parsedHost = parse_url($googleRedirect, PHP_URL_HOST);
+    if (is_string($parsedHost) && $parsedHost !== '') {
+        $hostFromEnv = $parsedHost;
+    }
+}
+
+$host = $_SERVER['HTTP_HOST'] ?? $hostFromEnv ?: 'localhost';
+
+$scheme = (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']))
+    ? trim(explode(',', $_SERVER['HTTP_X_FORWARDED_PROTO'])[0])
+    : ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http');
+
+$scriptName = $_SERVER['SCRIPT_NAME'] ?? '';
+$scriptDir  = '';
+
+if ($scriptName !== '') {
+    $scriptDir = str_replace('\\', '/', dirname($scriptName));
+    if ($scriptDir === '.' || $scriptDir === '/') {
+        $scriptDir = '';
+    } else {
+        $scriptDir = '/' . ltrim($scriptDir, '/');
+    }
+}
+
+$baseUrl = sprintf('%s://%s%s', $scheme, $host, $scriptDir === '' ? '/' : $scriptDir . '/');
+
+$oid = new LightOpenID($host);
+$oid->returnUrl = $baseUrl . 'steam_callback.php';
 
 if (! $oid->mode) {
     header('Location: index.php#authModal');
--- steam_login.php
+++ steam_login.php
@@ -2,15 +2,45 @@
 require_once __DIR__ . '/../config/config.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 
+$hostFromEnv = '';
+$googleRedirect = $_ENV['GOOGLE_REDIRECT_URI'] ?? getenv('GOOGLE_REDIRECT_URI') ?: '';
+
+if ($googleRedirect !== '') {
+    $parsedHost = parse_url($googleRedirect, PHP_URL_HOST);
+    if (is_string($parsedHost) && $parsedHost !== '') {
+        $hostFromEnv = $parsedHost;
+    }
+}
+
+$host = $_SERVER['HTTP_HOST'] ?? $hostFromEnv ?: 'localhost';
+
+$scheme = (!empty($_SERVER['HTTP_X_FORWARDED_PROTO']))
+    ? trim(explode(',', $_SERVER['HTTP_X_FORWARDED_PROTO'])[0])
+    : ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off') ? 'https' : 'http');
+
+$scriptName = $_SERVER['SCRIPT_NAME'] ?? '';
+$scriptDir  = '';
+
+if ($scriptName !== '') {
+    $scriptDir = str_replace('\\', '/', dirname($scriptName));
+    if ($scriptDir === '.' || $scriptDir === '/') {
+        $scriptDir = '';
+    } else {
+        $scriptDir = '/' . ltrim($scriptDir, '/');
+    }
+}
+
+$baseUrl = sprintf('%s://%s%s', $scheme, $host, $scriptDir === '' ? '/' : $scriptDir . '/');
+
 if (($_POST['csrf_token'] ?? '') !== ($_SESSION['csrf_token'] ?? '')) {
     $_SESSION['flash'] = ['alerts' => ['keys' => ['authInvalidCsrf'], 'mode' => 'modal']];
     header('Location: index.php#authModal');
     exit;
 }
 
-$oid = new LightOpenID(parse_url(BASE_URL, PHP_URL_HOST));
+$oid = new LightOpenID($host);
 $oid->identity  = 'https://steamcommunity.com/openid';
-$oid->returnUrl = BASE_URL . 'steam_callback.php';
+$oid->returnUrl = $baseUrl . 'steam_callback.php';
 $oid->required  = ['steamid'];
 
 header('Location: ' . $oid->authUrl());
